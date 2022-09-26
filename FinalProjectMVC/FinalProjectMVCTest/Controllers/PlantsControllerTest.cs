63d00dc8f512f256de02fd85bc1e6ad2b7fbecd6
Taif Bin Eid <taif.eid.98@gmail.com>
2022-09-26 23:44:06 +0300

    Add project slo

---
+++ FinalProjectMVC/FinalProjectMVCTest/Controllers/PlantsControllerTest.cs
@@ -0,0 +1,59 @@
+﻿using FinalProjectMVC.Controllers;
+using FinalProjectMVC.Data;
+using FinalProjectMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectMVCTest.Controllers
+{
+    [TestFixture]
+    class PlantsControllerTest
+    {
+        [Test] // test Edit2 action Plant Controller
+        public void Edit2_Return__NotFound_View_When_Id_Is_Null()
+        {
+            // Arrange
+            var controller = new PlantsController(null);
+            //Act
+            ViewResult viewResultObj = controller.Edit2(id: null) as ViewResult;
+            //Assert
+            NUnit.Framework.Assert.AreEqual("_NotFound", viewResultObj.ViewName);
+        }
+
+        [Test]
+        public void Execute_Should_Return_All_Plants()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "plantTest").Options;
+            var context = new ApplicationDbContext(options);
+            Seed(context);
+            //Act
+            var query = new PlantsController(context);
+            var result = query.Execute();
+            //Assert
+            /*context.Database.EnsureDeleted();*/
+            Assert.AreEqual(5, result.Count);
+        }
+
+        private void Seed(ApplicationDbContext context)
+        {
+            var plants = new[]
+            {
+             new PlantModel {PlantName="Rosemary"},
+             new PlantModel {PlantName="Basil"},
+             new PlantModel {PlantName="Fix"},
+             new PlantModel {PlantName="Kaluna"},
+             new PlantModel {PlantName="Olive Tree"},
+            };
+            context.Plants.AddRange(plants);
+            context.SaveChanges();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Plants-Project
+
+# Plant Store
+
+## Idea of project :
+ Plant store where it displays a range of plant species, and CRUD operations can be applied either by deleting, modifying, or displaying the details of the plant, As for the Create opertation, it was applied throug e-mail as it creates a new message and is saved in the database. 
+
+
+
+
+### ERD
+ > ERD 
+![Tuwaiq](./ERD/ERD.png)
+
+### Demo  
+ 
+
+
+https://user-images.githubusercontent.com/82478321/122554323-7fc4fd00-d041-11eb-9b99-c3dc5c5f34b8.mp4
+
+
+
+### Wireframe  
+
+ <div dir="ltr" align="center">
+
+
+> Home page 
+![Tuwaiq](./wireframe/homepage.png)
+>Registration page 
+![Tuwaiq](./wireframe/reg.png)
+>login page 
+![Tuwaiq](./wireframe/login.png)
+>Plant page
+![Tuwaiq](./wireframe/plant.png)
+>About page
+![Tuwaiq](./wireframe/about.png)
+>Contact page 
+![Tuwaiq](./wireframe/contact.png)
+>Location page
+![Tuwaiq](./wireframe/location.png)
+
+  </div>
+
+### Set up  
+### Prerequisites
+- NET 5 
+- ASP.NET MVC
+- Microsoft SQL Server 
+### Set up  
+ #### Database
+ ``` dotnet ef database update```
+### Front-End  
+ - HTML
+ - CSS
+ - JS
+ - Bootstrap 
+### Back-End 
+ - ASP.NET MVC
+ - MSSQL Server
+ - EF Core
+ - Google Map API
+
+### Author
+Taif Bin Eid
+</div>
